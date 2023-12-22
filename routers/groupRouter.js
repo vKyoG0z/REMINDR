3d775c4f1a7a7cf7cf38200cfc0584601fad5dd1
julien REYNAUD <julienjulienreynaud@outlook.fr>
2023-12-22 12:48:30 +0000

    début creatio group schema.prisma completer relation faite bug .create creation groupe

--- routers/groupRouter.js
+++ routers/groupRouter.js
@@ -3,22 +3,25 @@ const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-router.post('/group', async (req, res) => {
+router.post('/create-group', async (req, res) => {
   const { groupName } = req.body;
+  const userId = req.session.userId; // Obtient l'ID de l'utilisateur connecté depuis la session
 
   try {
     const newGroup = await prisma.group.create({
       data: {
         name: groupName,
         // Autres détails du groupe à enregistrer
+        members: { connect: { id: userId } } // Associe l'utilisateur au groupe
       },
     });
 
-    res.redirect(`/group.html?groupName=${encodeURIComponent(newGroup.name)}`);
+    res.redirect('/dashboard'); // Redirige vers le tableau de bord après la création du groupe
   } catch (error) {
     console.error(error);
     res.status(500).send('Erreur lors de la création du groupe');
   }
 });
+
 
 module.exports = router;
